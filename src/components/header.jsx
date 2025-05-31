5782527d3c4a5b1d477a301751141485210e526e
Macomi27 <macomfortitcham@gmail.com>
2025-05-31 23:22:08 +0100

    rest-countries-api-with-color-theme-switcher project setup

---
+++ src/components/header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './header.css'
+
+const Header = () => {
+  return (
+    <header className="header">
+      <h1>Country Information</h1>
+      <p>Explore countries around the world</p>
+    </header>
+  );
+}
+export default Header;
---
+++ src/pages/countries.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Header from "../components/header";
+
+const Countries = () => {
+  return (
+    <div className="countries">
+      <Header />
+    </div>
+  );
+}
+export default Countries;
